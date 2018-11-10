8a078a75950f56e7fde488b4ed0e9c92c4d1cffb
RicardoFagundes123 <rlf4@cin.ufpe.br>
2018-11-09 23:03:32 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view){
+        TextView texto = findViewById(R.id.text2);
+        int n = new Random().nextInt(10);
+        texto.setText("Numero selecionado: " + n);
+
+    }
+
+}
